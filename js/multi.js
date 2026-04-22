40ced66ceac4216db1542427cdb4b20cbf44e76c
M'bark LAHNOUD <lhnoudmbark6@gmail.com>
2026-04-22 13:39:33 +0000

    Update shuffle multi.js

--- js/multi.js
+++ js/multi.js
@@ -281,7 +281,11 @@ function endGame() {
 /* ===================== */
 
 function shuffle(arr) {
-    return arr.sort(() => Math.random() - 0.5);
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
 }
 function restartGame() {
     location.reload();
